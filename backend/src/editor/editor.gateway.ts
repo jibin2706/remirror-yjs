9c12097da66ad2b9670a930c20dbcf0cf17f1ce0
Jibin Thomas <jibin.thomas2706@gmail.com>
2023-06-24 12:37:26 +0530

    multiple document editing

--- backend/src/editor/editor.gateway.ts
+++ backend/src/editor/editor.gateway.ts
@@ -2,6 +2,8 @@ import {
   WebSocketGateway,
   WebSocketServer,
   OnGatewayConnection,
+  OnGatewayDisconnect,
+  WsException,
 } from '@nestjs/websockets';
 import { Server } from 'ws';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -11,15 +13,25 @@ import { setupWSConnection } from 'y-websocket/bin/utils';
 import { EditorService } from './editor.service';
 
 @WebSocketGateway(8000, { path: 'yjs' })
-export class EditorGateway implements OnGatewayConnection {
+export class EditorGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
   constructor(private readonly editorService: EditorService) {}
 
   handleConnection(connection: WebSocket, request: Request) {
-    console.log('connection request made');
-    const docName = 'test';
-    setupWSConnection(connection, request, { ...(docName && { docName }) });
+    const queryParams = new URLSearchParams(request.url.split('?')[1]);
+    const docName = queryParams.get('docId');
+
+    console.log('connection request, docId:', docName);
+
+    if (!docName) {
+      throw new WsException('invalid document id');
+    }
+    setupWSConnection(connection, request, { docName });
+  }
+
+  handleDisconnect(connection: WebSocket) {
+    console.log('disconnected');
   }
 }
